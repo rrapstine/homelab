fe03651441b68ae671d14ab75a6885746a0f161c
Richard Rapstine <richard.rapstine@gmail.com>
2025-06-12 02:56:41 -0500

    Implementing Caddy container and refactoring some of the structure

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -24,8 +24,41 @@ variable "ssh_private_key_path" {
 variable "services" {
   description = "Map of service configurations for dynamic deployment"
   type = map(object({
-    enabled = bool
-    port    = number
-    version = string
+    enabled           = bool
+    internal_app_port = optional(number)
+    direct_exposed_ports = optional(list(object({
+      internal = number
+      external = number
+      protocol = optional(string, "tcp")
+    })))
   }))
+}
+
+# Networking configuration
+variable "local_domain" {
+  description = "The fully qualified domain name of the server"
+  type        = string
+}
+
+variable "container_network_name" {
+  description = "The name of the shared docker network"
+  type        = string
+}
+
+# Caddy configuration
+variable "caddy_config_host_path" {
+  description = "The full path on the host system where Terraform will generate the Caddyfile"
+  type        = string
+}
+
+variable "caddy_http_port" {
+  description = "The port Caddy will use for HTTP traffic"
+  type        = number
+  default     = 80
+}
+
+variable "caddy_https_port" {
+  description = "The port Caddy will use for HTTPS traffic"
+  type        = number
+  default     = 443
 }
